4d4bc4c7a51716b28d8c9fb5417334ba11d5bc10
JCPrime01 <vanzokiller@gmail.com>
2026-06-18 21:34:46 -0300

    chore: shareCode LHEAXOGR3KA -> Q4N212A9JU8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104&shareCode=LHEAXOGR3KA&afp4=bot' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104&shareCode=Q4N212A9JU8&afp4=bot' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
