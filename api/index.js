5422a42898d636e6bf08859afb6692203f2ee494
JCPrime01 <vanzokiller@gmail.com>
2026-06-21 22:13:43 -0300

    chore: shareCode ZB48YUK42K7 -> ON51UVRLQVP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104&shareCode=ZB48YUK42K7&afp4=bot' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104&shareCode=ON51UVRLQVP&afp4=bot' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
