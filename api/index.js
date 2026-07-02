7ee7196bdd448986baf078cd21e3e3dbc16ffaeb
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 22:27:49 -0300

    chore: shareCode U6US8R3B935 -> WIIF79R67S9 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=U6US8R3B935'
+    const shareCode = '&shareCode=WIIF79R67S9'
     
     const afp = '&afp4=bot'
     
