9661e7566d903ac1cb894440d056ddc7ff99135f
JCPrime01 <vanzokiller@gmail.com>
2026-07-08 10:10:39 -0300

    chore: shareCode KRTCKCZ3HXR -> GUOP5B88W64 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=KRTCKCZ3HXR'
+    const shareCode = '&shareCode=GUOP5B88W64'
     
     const afp = '&afp4=bot'
     
