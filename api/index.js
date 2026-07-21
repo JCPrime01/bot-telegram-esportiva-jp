54c7cc314c6980e810ab891eba430f029bceb519
JCPrime01 <vanzokiller@gmail.com>
2026-07-20 23:27:20 -0300

    chore: shareCode 2EDOHDD86T9 -> HVZKJURTNET (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=2EDOHDD86T9'
+    const shareCode = '&shareCode=HVZKJURTNET'
     
     const afp = '&afp4=bot'
     
