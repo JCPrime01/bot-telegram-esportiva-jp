eb681166f8aa94b7f60717e58cc8d9f925adbb5f
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 23:15:16 -0300

    chore: shareCode HVZKJURTNET -> EETW589DLXS (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=HVZKJURTNET'
+    const shareCode = '&shareCode=EETW589DLXS'
     
     const afp = '&afp4=bot'
     
