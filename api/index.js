c005273a7750e228067edc26004643450a85c890
JCPrime01 <vanzokiller@gmail.com>
2026-08-07 20:25:20 -0300

    chore: shareCode EETW589DLXS -> JNWI23CT21K (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=EETW589DLXS'
+    const shareCode = '&shareCode=JNWI23CT21K'
     
     const afp = '&afp4=bot'
     
