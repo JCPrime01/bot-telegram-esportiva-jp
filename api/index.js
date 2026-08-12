973b06a13ba58d32a93701fa44bd579067d84e01
JCPrime01 <vanzokiller@gmail.com>
2026-08-12 00:38:10 -0300

    chore: shareCode GDS81GQX895 -> T6JBTQ8LJJK (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=GDS81GQX895'
+    const shareCode = '&shareCode=T6JBTQ8LJJK'
     
     const afp = '&afp4=bot'
     
