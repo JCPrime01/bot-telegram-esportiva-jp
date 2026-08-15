669b46a435d8e8fcfc3f5e54a37523f06638beff
JCPrime01 <vanzokiller@gmail.com>
2026-08-14 23:36:31 -0300

    chore: shareCode HNBL22KYOYJ -> C6XKKB77SS0 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=HNBL22KYOYJ'
+    const shareCode = '&shareCode=C6XKKB77SS0'
     
     const afp = '&afp4=bot'
     
