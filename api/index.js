513f66db639914292fe016785ee56bf12593a3e9
JCPrime01 <vanzokiller@gmail.com>
2026-09-06 00:47:00 -0300

    chore: shareCode P46BITDIFFU -> 2HPKRCA15MD (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=P46BITDIFFU'
+    const shareCode = '&shareCode=2HPKRCA15MD'
     
     const afp = '&afp4=bot'
     
