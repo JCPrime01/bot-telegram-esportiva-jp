51f34269492bb1f13c4f4ee7a549ec226f684886
Devgrupojc <devgrupojc@gmail.com>
2026-09-08 10:45:31 -0300

    chore: shareCode 2HPKRCA15MD -> ST6FBQMU59N (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=2HPKRCA15MD'
+    const shareCode = '&shareCode=ST6FBQMU59N'
     
     const afp = '&afp4=bot'
     
