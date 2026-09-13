55c25e43567c01a8768ed873215ec0d24600f2d1
Devgrupojc <devgrupojc@gmail.com>
2026-09-13 00:21:45 -0300

    chore: shareCode WLUCF5RHPDX -> WZMZMQ49XD6 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=WLUCF5RHPDX'
+    const shareCode = '&shareCode=WZMZMQ49XD6'
     
     const afp = '&afp4=bot'
     
