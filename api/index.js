9c4f54aeb56bef4ed912c4f34eb3aa2f69eba11d
Devgrupojc <devgrupojc@gmail.com>
2026-09-16 01:09:42 -0300

    chore: shareCode 684ZQKS4925 -> BQ60S5C2WPU (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=684ZQKS4925'
+    const shareCode = '&shareCode=BQ60S5C2WPU'
     
     const afp = '&afp4=bot'
     
