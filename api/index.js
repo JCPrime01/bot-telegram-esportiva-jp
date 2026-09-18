9031ce7bce7f01006cf9fb3041253480d3747db1
Devgrupojc <devgrupojc@gmail.com>
2026-09-18 04:01:52 -0300

    chore: shareCode OP3FIH3UFZC -> NK8IW62R5EM (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=30104'
 
-    const shareCode = '&shareCode=OP3FIH3UFZC'
+    const shareCode = '&shareCode=NK8IW62R5EM'
     
     const afp = '&afp4=bot'
     
